1ae04a15d7ff7ce08900bcb9786be3ad22685029
RonaldButron <ronaldbutron@gmail.com>
2015-12-03 11:26:02 -0400

    Push Page Object and Page Transporter

---
+++ src/main/java/ui/BasePageObject.java
@@ -0,0 +1,28 @@
+package ui;
+
+import framework.DriverManager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+/**
+ * User: RonaldButron
+ * Date: 12/03/15
+*/
+public abstract class BasePageObject {
+     protected WebDriver driver;
+     protected WebDriverWait driverWait;
+
+    public BasePageObject(){
+        this.driver = DriverManager.getInstance().getWebDriver();
+        this.driverWait = DriverManager.getInstance().getWait();
+        PageFactory.initElements(driver, this);
+    }
+
+    public abstract void waitUntilPageObjectIsLoaded();
+
+
+
+}
---
+++ src/main/java/ui/PageTransporter.java
@@ -0,0 +1,67 @@
+package ui;
+
+import framework.DriverManager;
+import org.openqa.selenium.WebDriver;
+import ui.pages.LoginPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import utils.ConfigFileReader;
+
+/**
+  * User: RonaldButron
+ * Date: 12/03/15
+ */
+public class PageTransporter {
+
+    private WebDriver driver = DriverManager.getInstance().getWebDriver();
+    private ConfigFileReader reader = new ConfigFileReader();
+    private  String URLLogin = reader.getPropertiesValues("URLLogin");
+    private static PageTransporter instance;
+
+    protected PageTransporter(){
+            init();
+    }
+
+    public static PageTransporter getInstance(){
+                if(instance == null){
+                    instance = new PageTransporter();
+                }
+        return instance;
+    }
+
+    private void init(){
+
+    }
+
+    /**
+     * This method navigate to the Login URL
+     * @param URL
+     */
+    private void goToURL(String URL){
+        driver.navigate().to(URL);
+    }
+
+    /**
+     * This method obtain the current URL
+     * @return a string with the URL
+     */
+    public String getCurrentURL(){
+
+        return driver.getCurrentUrl();
+
+    }
+
+    /**
+     * This method go to the Login Page
+     * @return a new Login Page
+     */
+    public LoginPage toLoginPage(){
+
+        goToURL(URLLogin);
+        return new LoginPage();
+
+
+
+    }
+
+}
