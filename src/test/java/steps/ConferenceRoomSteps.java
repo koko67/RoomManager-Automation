2282b77a78386ebb62f203965d4c12cde135e49e
Jorge Avila Baldiviezo <jorge.avila@fundacion-jala.org>
2015-12-10 12:04:54 -0400

    updated to use the Entity in Conference room steps

--- src/test/java/steps/ConferenceRoomSteps.java
+++ src/test/java/steps/ConferenceRoomSteps.java
@@ -20,8 +20,9 @@ public class ConferenceRoomSteps {
 
     private ConferenceRoom conferenceRoom;
 
-    public ConferenceRoomSteps(){
+    public ConferenceRoomSteps(ConferenceRoom conferenceRoom){
         homePage = new HomePage();
+        this.conferenceRoom = conferenceRoom;
     }
 
     @Given("^I open the Room \"(.*?)\" from the Conference Room$")
@@ -44,7 +45,10 @@ public class ConferenceRoomSteps {
 
     @When("^I edit the following info: Display Name \"(.*?)\", code \"(.*?)\" and capacity \"(.*?)\"$")
     public void editInfoConferenceRoom(String displayName, String roomCode, String roomCapacity){
-        roomSettingsPage.fillForm(displayName, roomCode, roomCapacity);
+        conferenceRoom.setCustomDisplayName(displayName);
+        conferenceRoom.setCode(roomCode);
+        conferenceRoom.setCapacity(roomCapacity);
+        roomSettingsPage.fillForm(conferenceRoom);
     }
 
     @Then("^the info edited should be obtained by API request for the Room \"(.*?)\"$")
