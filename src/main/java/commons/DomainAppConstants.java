de9bd937aeae613efd4b7ba6f09f19ce5b13742e
RonaldButron <ronaldbutron@gmail.com>
2015-12-11 15:02:32 -0400

    Update Constants

--- src/main/java/commons/DomainAppConstants.java
+++ src/main/java/commons/DomainAppConstants.java
@@ -8,5 +8,5 @@ public class DomainAppConstants {
 
     public static final String INVALID_USER_ERROR = "Your username and/or password is incorrect.";
     public static final String MEETING_SUCCESSFULLY_CREATED = "Meeting successfully created";
-
+    public static final String DEFECT_ID = "RM01";
 }
