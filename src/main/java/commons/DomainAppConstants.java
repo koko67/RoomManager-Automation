a12b56200bac625e277545832691849abe824246
RonaldButron <ronaldbutron@gmail.com>
2015-12-11 14:37:12 -0400

    pushing Domain App Constants

---
+++ src/main/java/commons/DomainAppConstants.java
@@ -0,0 +1,12 @@
+package commons;
+
+/**
+ * User: Ronald Butron
+ * Date: 12/11/15
+ */
+public class DomainAppConstants {
+
+    public static final String INVALID_USER_ERROR = "Your username and/or password is incorrect.";
+    public static final String MEETING_SUCCESSFULLY_CREATED = "Meeting successfully created";
+
+}
