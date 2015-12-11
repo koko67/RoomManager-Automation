7a8acf6a0de89f9cb726de143c62ee33e289761f
RonaldButron <ronaldbutron@gmail.com>
2015-12-11 15:09:04 -0400

    Update Data base name constant

--- src/main/java/commons/DomainAppConstants.java
+++ src/main/java/commons/DomainAppConstants.java
@@ -9,4 +9,5 @@ public class DomainAppConstants {
     public static final String INVALID_USER_ERROR = "Your username and/or password is incorrect.";
     public static final String MEETING_SUCCESSFULLY_CREATED = "Meeting successfully created";
     public static final String DEFECT_ID = "RM01";
+    public static final String DB_SERVER_NAME = "roommanager";
 }
