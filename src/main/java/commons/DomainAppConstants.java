414e17ba09432a03a8c7571bb64da82aeb4d8157
RonaldButron <ronaldbutron@gmail.com>
2015-12-13 10:48:24 -0400

    Update Constants

--- src/main/java/commons/DomainAppConstants.java
+++ src/main/java/commons/DomainAppConstants.java
@@ -8,6 +8,8 @@ public class DomainAppConstants {
 
     public static final String MEETING_CONFLICT_ERROR = "There is a conflict with another meeting, please choose another time interval";
     public static final String MEETING_SUCCESSFULLY_CREATED = "Meeting successfully created";
+    public static final String MEETING_SUCCESSFULLY_UPDATED = "Meeting successfully updated";
+    public static final String MEETING_SUCCESFULLY_REMOVED = "Meeting successfully removed";
     public static final String DEFECT_ID = "RM01";
     public static final String DB_SERVER_NAME = "roommanager";
 }
