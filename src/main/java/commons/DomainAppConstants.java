195a56d60f99b27aaa604bf84f6b805bf7097798
RonaldButron <ronaldbutron@gmail.com>
2015-12-14 22:01:15 -0400

    insert new Constants

--- src/main/java/commons/DomainAppConstants.java
+++ src/main/java/commons/DomainAppConstants.java
@@ -12,4 +12,14 @@ public class DomainAppConstants {
     public static final String MEETING_SUCCESFULLY_REMOVED = "Meeting successfully removed";
     public static final String DEFECT_ID = "RM01";
     public static final String DB_SERVER_NAME = "roommanager";
+    public static final String COLLECT_MEETING = "meetings";
+    public static final String COLLECT_ROOMS = "rooms";
+    public static final String KEY_ID = "_id";
+    public static final String KEY_SERVICE_ID = "serviceId";
+    public static final String KEY_ROOM_ID = "roomId";
+    public static final String KEY_DISPLAY_NAME = "displayName";
+    public static final String KEY_TITLE = "title";
+    public static final String REPLACE_SERVICE_ID = "#serviceId#";
+    public static final String REPLACE_ROOM_ID = "#roomId#";
+    public static final String REPLACE_MEETING_ID = "#meetingId#";
 }
