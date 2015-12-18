766ad43b90956019c04c94bcfce10ae909038876
Jorge Avila Baldiviezo <jorge.avila@fundacion-jala.org>
2015-12-17 21:09:04 -0400

    updated the inheritance changes in the steps

--- src/test/java/steps/ConferenceRoomSteps.java
+++ src/test/java/steps/ConferenceRoomSteps.java
@@ -36,7 +36,6 @@ import java.util.List;
  */
 public class ConferenceRoomSteps {
 
-    private HomePage homePage;
     private ConferenceRoomsPage conferenceRoomsPage;
     private RoomSettingsPage roomInfoPage;
     private LocationPage locationPage;
@@ -48,7 +47,8 @@ public class ConferenceRoomSteps {
     private List<String> locationIds;
 
     public ConferenceRoomSteps(ConferenceRoom conferenceRoom){
-        homePage = new HomePage();
+        conferenceRoomsPage = new ConferenceRoomsPage();
+        locationPage = new LocationPage();
         resource = new Resource();
         this.conferenceRoom = conferenceRoom;
         this.location = new Location();
@@ -61,8 +61,7 @@ public class ConferenceRoomSteps {
         conferenceRoom.setCustomDisplayName(roomName);
         UIMethods.refreshPage();
         UIMethods.switchPages(LeftBarOptions.CONFERENCE_ROOMS.getToPage());
-        conferenceRoomsPage = homePage.getLeftMenuPanel()
-                                      .clickOnConferenceRooms();
+        conferenceRoomsPage.getLeftMenuPanel().clickOnConferenceRooms();
 
         roomInfoPage = conferenceRoomsPage.openConferenceRoomSettings(conferenceRoom.getCustomDisplayName());
     }
@@ -81,8 +80,7 @@ public class ConferenceRoomSteps {
 
     @Then("^the current room is associated to the Location defined in the Locations page$")
     public void isAssociatedRoomToLocationRoomPage(){
-        locationPage = homePage.getLeftMenuPanel()
-                .clickOnLocationPage();
+        locationPage.getLeftMenuPanel().clickOnLocationPage();
         boolean existAssociated = locationPage.verifyIfExistLocationAssociation(location, conferenceRoom);
         Assert.assertTrue(existAssociated);
     }
@@ -135,7 +133,7 @@ public class ConferenceRoomSteps {
 
     @And("^I go to Conference Room page$")
     public void goToConferenceRoomPage(){
-        conferenceRoomsPage = homePage.getLeftMenuPanel().clickOnConferenceRooms();
+        conferenceRoomsPage.getLeftMenuPanel().clickOnConferenceRooms();
     }
     @And("^I select the resource button in the header page$")
     public void displayResourceInTableConferenceRoom(){
@@ -148,7 +146,7 @@ public class ConferenceRoomSteps {
         conferenceRoom.setCustomDisplayName(roomName);
         UIMethods.refreshPage();
         UIMethods.switchPages(LeftBarOptions.CONFERENCE_ROOMS.getToPage());
-        conferenceRoomsPage = homePage.getLeftMenuPanel().clickOnConferenceRooms();
+        conferenceRoomsPage.getLeftMenuPanel().clickOnConferenceRooms();
         roomInfoPage = conferenceRoomsPage.openConferenceRoomSettings(conferenceRoom.getCustomDisplayName());
         resourceAssociatePage = roomInfoPage.clickOnResourceAssociateTab();
         resource.setQuantity(quantity);
